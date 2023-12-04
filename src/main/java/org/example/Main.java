254c42365a6c4b0bc5720313d44117003dcf2b60
afalarco22 <afalarco@unal.edu.co>
2023-12-04 15:35:12 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Entradas del problema
+        String nombreUsuario="Juan Esteban Zapata";
+        short edadUsuario=34;
+        long  telefonoUsuario=3044578600L;
+        String documentoIdentidad="1000638022";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario="Cra 39# 70-93";
+        double costoInscriocion=15000.0;
+        float tasaInteresNatillera=0.1F;
+        double saldoFinalUsuario;
+
+        //PROCESO
+
+        //1. Asignar un valor especifico a una variable
+        //desde el TECLADO (Recibir datos)
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu nombre?");
+        leerDato.nextLine();
+    }
+}
